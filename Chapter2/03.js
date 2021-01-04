eed4336338d1793a73b210e7a24a3f06f7ccda7f
lee-hyunsu <dlgustn675@naver.com>
2021-01-04 14:33:05 +0900

    study

---
+++ Chapter2/03.js
@@ -0,0 +1,6 @@
+console.log(Math.abs(-10)); /*-10의 절대값은 10*/
+console.log(Math.pow(2, 10)); /*2의 10승은 1024*/
+console.log(Math.sign(2)); /*2는 양수이므로 1*/
+console.log(Math.sign(-2)); /*-2는 음수이므로 -1*/
+console.log(Math.sqrt(16)); /*16의 제곱근은 4*/
+console.log(Math.log(Math.E)); /*로그 e는 1*/
