cd4f341bdcc73558148681254bc131904276e81b
lee-hyunsu <dlgustn675@naver.com>
2021-01-04 12:55:19 +0900

    study

---
+++ Chapter2/02.js
@@ -0,0 +1,7 @@
+//0이상 1미만 임의의 소수
+console.log(Math.random());
+//0이상 100미만 임의의 정수
+console.log(Math.floor(Math.random() * 100));
+//10이상 20미만 임의의 정수
+console.log(10 + Math.floor(Math.random() * 10));
+
