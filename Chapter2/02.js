35b242914fa56a6737bd766338b00e609e8ead4c
lee-hyunsu <dlgustn675@naver.com>
2021-01-04 13:38:42 +0900

    study

--- Chapter2/02.js
+++ Chapter2/02.js
@@ -5,3 +5,19 @@ console.log(Math.floor(Math.random() * 100));
 //10이상 20미만 임의의 정수
 console.log(10 + Math.floor(Math.random() * 10));
 
+//직사각형을 불러오고,
+const rectangle = document.querySelector('.rectangle');
+
+//버튼 클릭 시 onClickButton() 실행
+document.querySelector('.button')
+addEventListener('click', onClickButton);
+
+function onClickButton() {
+    const randomHue = Math.trunc(Math.random() * 360);
+
+    const randomColorStart = `hsl(${randomHue}, 100%, 50%)`;
+    const randomColorEnd = `hsl(${randomHue + 40}, 100%, 50%)`;
+
+    rectangle.style.setProperty('--start', randomColorStart);
+    rectangle.style.setProperty('--end', randomColorEnd);
+}
