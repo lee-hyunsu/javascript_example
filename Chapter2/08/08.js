c3f0ee451d8f10febe1669b530300f619a37e549
lee-hyunsu <dlgustn675@naver.com>
2021-01-04 16:11:35 +0900

    study

---
+++ Chapter2/08/08.js
@@ -0,0 +1,13 @@
+const myString = 'JavaScript를 배우자';
+
+//includes는 문자열의 포함 여부를 확인
+const a1 = myString.includes('JavaScript');
+console.log(a1);
+
+//startsWith는 문자열 시작 문자인지 확인
+const a2 = myString.startsWith('배우자');
+console.log(a2);
+
+//endsWith는 문자열 종료 문자인지 확인
+const a3 = myString.endsWith('배우자');
+console.log(a3);
