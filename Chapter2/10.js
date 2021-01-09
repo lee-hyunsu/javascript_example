50138df06890a49d18a588170f396ae793f10d5c
lee-hyunsu <dlgustn675@naver.com>
2021-01-09 23:15:13 +0900

    study

---
+++ Chapter2/10.js
@@ -0,0 +1,10 @@
+console.log('JavaScript'.slice(0, 4)); //Java
+console.log('JavaScript'.slice(0)); //JavaScript
+console.log('JavaScript'.substring(0, 4)); //Java
+console.log('JavaScript'.substring(0)); //JavaScript
+
+console.log('나의 노트북'.slice(3, -1)); //노트
+console.log('나의 노트북'.slice(-6, -4)); //나의
+
+console.log('나의 노트북'.substring(3, -3)); //나의
+console.log('나의 노트북'.substring(-4, -1)); //공백
