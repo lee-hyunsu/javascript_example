c5fd233bf346ee78d21137a64c73b93bd06ae04a
lee-hyunsu <dlgustn675@naver.com>
2021-01-12 17:54:54 +0900

    test

--- Chapter2/11/11.js
+++ Chapter2/11/11.js
@@ -1,5 +1,14 @@
-const imageName = 'image1.png';
-imageName.replace('1.png', '2.png');
+//문자열을 다른 문자열로 바꾸고 싶을 때
+//문자열 내 불필요한 줄바꿈 코드를 <br>로 바꾸고 싶을 때
+//빈칸을 제거하고 싶을 때
 
+//image1.png를 image2.png로 바꿈
+const imageName = 'image1.png';
+imageName.replace('1.png', '2.png'); //image2.png로 바꿈
+
+//문자열 내 줄바꿈 코드를 제거
 const inputText = '제이/n-펍';
-inputText.replace('/n', '');
+inputText.replace('/n', ''); //제이-펍
+
+let phoneNumber = '010-7587-3955';
+phoneNumber.replace('-', ''); //0107587-3955 출력
