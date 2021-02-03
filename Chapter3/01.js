35e3fc9780a93c048a2633abeb0d2f4f86313b7d
lee-hyunsu <dlgustn675@naver.com>
2021-02-03 17:33:12 +0900

    study

---
+++ Chapter3/01.js
@@ -0,0 +1,31 @@
+//배열 정의!!!
+
+const array1 = []; // 빈 배열
+console.log(array1);
+
+const array2 = [0, 2, 8] //'0', '2', '8'이 들어있는 데이터 타입
+console.log(array2);
+
+const array3 = ['곰', '여우'] //'곰', '여우'가 들어있는 데이터 타입
+console.log(array3);
+
+const array4 =[1, '곰' , false]; //'1', '곰', 'false'가 들어있는 데이터 타입
+console.log(array4);
+
+const array5 = [[1, 1, 1], [2, 2, 2]]; //[1, 1, 1], [2, 2, 2]가 들어있는 데이터 타입
+console.log(array5);
+
+const array6 = [{ id: 1, name: '곰'}, { id: 2, name: '고양이'}]; //두 개의 객체가 들어있는 데이터 타입
+console.log(array6);
+
+const array7 = ['곰', '여우', '고양이'];
+console.log(array7[0]);
+console.log(array7[1]);
+console.log(array7[2]);
+
+const array8 = new Array('곰', '여우');
+console.log(array8[0]);
+console.log(array8[1]);
+
+const array9 = new Array(10); //10개의 데이터를 넣을 수 있음
+console.log(array9[0]);
---
+++ Chapter3/02.js
@@ -0,0 +1,7 @@
+//배열의 길이 확인하기
+
+const array1 = ['곰', '여우', '사자'];
+console.log(array1.length);
+
+const array2 = [{ id: 1, name: '곰'}, { id: 2, name: '여우'}];
+console.log(array2.length);
