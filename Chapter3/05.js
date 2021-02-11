34fb55d37ee35480737fe9c2f684962602a88ee2
lee-hyunsu <dlgustn675@naver.com>
2021-02-11 23:09:32 +0900

    study

---
+++ Chapter3/05.js
@@ -0,0 +1,10 @@
+const array = ['딸기', '귤', '사과'];
+
+//배열의 길이 가져오기
+const arrayLength = array.length;
+
+//배열의 요소 데이터 처리
+for(let i = 0; i < arrayLength; i++) {
+    //인덱스 i의 요소 출력
+    console.log(array[i]);
+}
---
+++ Chapter3/06.js
@@ -0,0 +1,7 @@
+const array1 = ['사과', '귤'];
+array1.unshift('바나나'); //'바나나'를 배열 첫 위치에 추가
+console.log(array1);
+
+const array2 = ['사과', '귤'];
+array2.push('바나나', '딸기'); //'바나나'와 '딸기'를 배열 마지막에 추가
+console.log(array2);
