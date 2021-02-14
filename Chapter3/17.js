d73c80fdbaf32245bcb446657d334b1c256f586e
lee-hyunsu <dlgustn675@naver.com>
2021-02-14 15:38:53 +0900

    study

--- Chapter3/17.js
+++ Chapter3/17.js
@@ -1,4 +1,18 @@
 const idList = [4, 10, 20];
-
 const userIdList = idList.map((value) => `userid_${value}`);
 console.log(userIdList);
+
+
+const idList1 = [3, 8, 12];
+const userIdList1 = idList1.map((value, index) => `userid_${index+1}_${value}`);
+console.log(userIdList1);
+
+const apiResponseData = [
+    {id: 1, name: '곰'},
+    {id: 2, name: '사자'},
+    {id: 3, name: '호랑이'}
+];
+
+const idList2 = apiResponseData.map((value) => value.id);
+
+console.log(idList2);
