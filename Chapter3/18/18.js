c3acabfe0379df8d3404b262a35332b2a54b5b84
lee-hyunsu <dlgustn675@naver.com>
2021-02-15 14:59:08 +0900

    study

--- Chapter3/18/18.js
+++ Chapter3/18/18.js
@@ -12,3 +12,21 @@ const newArray2 = [10, 20, 30 ,40].filter(function(value) {
 
 console.log(newArray1);
 console.log(newArray2);
+
+const userDataList = [
+    {name: '곰', age: 18},
+    {name: '사자', age: 31},
+    {name: '호랑이', age: 46},
+    {name: '강아지', age: 27},
+    {name: '고양이', age: 42},
+    {name: '생쥐', age: 22},
+    {name: '기린', age: 19},
+    {name: '얼룩말', age: 35},
+];
+
+//.button 요소의 이벤트 설정
+document.querySelector('.button').forEach((element) => {
+    element.addEventListener('click', (event) => {
+        onClickButton(event);
+    });
+});
